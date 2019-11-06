4b2c50734037ab3a7c9f542537d6ca0b5d5fafb1
waldinho <james.waller1984@gmail.com>
2019-11-07 10:04:37 +1100

    working counter, change to get working with test API later.

---
+++ sagas.js
@@ -0,0 +1,22 @@
+import { put, takeEvery, all, call } from 'redux-saga/effects'
+
+export const delay = (ms) => new Promise(res => setTimeout(res, ms))
+
+function* helloSaga() {
+  console.log('Hello Sagas!')
+}
+
+export function* incrementAsync() {
+  yield call(delay, 1000)
+  yield put({ type: 'INCREMENT' })
+}
+function* watchIncrementAsync() {
+  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+}
+
+export default function* rootSaga() {
+  yield all([
+    helloSaga(),
+    watchIncrementAsync()
+  ])
+}
